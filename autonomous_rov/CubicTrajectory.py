acda766eee74f69add61656b303d65ac851146ad
ppakr <akira.techap@gmail.com>
2025-03-11 14:05:02 +0700

    refactor: update cubic trject class and add gitignore

--- autonomous_rov/CubicTrajectory.py
+++ autonomous_rov/CubicTrajectory.py
@@ -1,28 +1,32 @@
 import numpy as np
 
-class CubicTrajectory():
-    def __init__(self, time_init, time_final, z_init=0.0, z_final=0.5, dt=0.1):
-        self.time_init = time_init
-        self.time_final = time_final
+class CubicTrajectory:
+    def __init__(self, z_init=0.0, z_final=0.5):
+        """
+        Initialize cubic trajectory parameters.
+        
+        :param z_init: Initial depth position
+        :param z_final: Final depth position
+        """
         self.z_init = z_init
         self.z_final = z_final
-        self.dt = dt
-        self.time_values = np.arange(self.time_init, self.time_final , self.dt) 
-        self.z_desired = []
-        self.z_dot_desired = []
-        self.a2 = (3.0 * (self.z_final - self.z_init) / (self.time_final**2))
-        self.a3 = (-2.0 * (self.z_final - self.z_init) / (self.time_final**3))
+
+    def get_waypoint(self, t, time_init, time_final):
+        """
+        Compute the desired depth (z) and velocity (z_dot) for a given time t.
         
-    def generateCubicTrajectory(self):
-        for i in self.time_values:
-            if i < self.time_final:
-                z = self.z_init + (self.a2 * i**2) + (self.a3 * i**3)
-                z_dot = self.z_init + (2 * self.a2 * i) + (3 * self.a3 * i**2)
-                self.z_dot_desired.append(z_dot)
-                self.z_desired.append(z)
-            elif i >= self.time_final: 
-                self.z_desired.append(self.z_final)
-                self.z_dot_desired.append(0.0)
-            else:
-                print("Error")
-        return self.z_desired, self.z_dot_desired
+        :param t: Current time in seconds
+        :param time_init: Start time of the trajectory
+        :param time_final: End time of the trajectory
+        :return: (z, z_dot)
+        """
+        if t < time_init:
+            return self.z_init, 0.0
+        elif t < time_final:
+            a2 = (3.0 * (self.z_final - self.z_init) / (time_final**2))
+            a3 = (-2.0 * (self.z_final - self.z_init) / (time_final**3))
+            z = self.z_init + (a2 * (t - time_init)**2) + (a3 * (t - time_init)**3)
+            z_dot = (2 * a2 * (t - time_init)) + (3 * a3 * (t - time_init)**2)
+            return z, z_dot
+        else:
+            return self.z_final, 0.0
